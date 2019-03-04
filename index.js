95de1bde0441131fbff34cbe693f945725c9ca88
Jonathan Sellam <jsellam@outbrain.com>
2019-03-04 10:01:20 +0200

    All methods

--- index.js
+++ index.js
@@ -1,64 +1,235 @@
-const hson_package = require('./lib/hson_wasm');
+const { TextDecoder, TextEncoder } = require('util');
 
-rust.then(m => {
-  console.log(m.parse(`{
-    "div": {
-      "attrs": {
-        "class": ["active", 123, 0.25864, "test"],
-        "onClick": "doSomething"
-      },
-      "div": {
-        "p": {
-          "attrs": {
-            "id": 12,
-            "rate": 0.4321,
-            "trusted": true,
-            "text": "foo"
-          },
-          "span": {
-            "text": "Hello"
+class Hson {
+  constructor () {
+    this.is_node = (typeof module !== 'undefined' && module.exports);
+    this.instance = null;
+    this.memory = null;
+  }
+
+  instantiate () {
+    return new Promise((resolve, reject) => {
+      if (this.is_node) {
+        const fs = require('fs');
+        const path = require('path');
+
+        const src_path = path.resolve(__dirname, 'lib', 'hson.wasm');
+        const source = fs.readFileSync(src_path);
+        const typedArray = new Uint8Array(source);
+        const env = {
+          memoryBase: 0,
+          tableBase: 0,
+          memory: new WebAssembly.Memory({
+            initial: 256
+          }),
+          table: new WebAssembly.Table({
+            initial: 0,
+            element: 'anyfunc'
+          }),
+          console: (ptr) => {
+            let str = this.copyCStr(ptr);
+            console.log(str);
           }
-        },
-        "p": {
-          "span": {
-            "text": "World"
-          }
-        }
-      },
-      "div": {
-        "component": "test",
-        "attrs": {},
-        "onClick": "componentDoSomething"
+        };
+
+        WebAssembly.instantiate(typedArray, {
+          env: env
+        }).then(result => {
+          this.instance = result.instance.exports;
+          this.instance.debug();
+          resolve();
+        }).catch(reject);
+      } else {
+
+      }
+    });
+  }
+
+  parse (data_str) {
+    try {
+      const result = JSON.parse(this.copyCStr(this.instance.parse(this.newString(data_str))));
+      if (result.status === 'ERR') console.error(result.reason);
+      return (result.status === 'OK' && result.data);
+    } catch (e) {
+      console.error(e);
+      return false;
+    }
+  }
+
+  stringify () {
+    try {
+      const result = JSON.parse(this.copyCStr(this.instance.stringify()));
+      if (result.status === 'ERR') console.error(result.reason);
+      return (result.status === 'OK') ? result.data : null;
+    } catch (e) {
+      console.error(e);
+      return null;
+    }
+  }
+
+  search (search_str) {
+    try {
+      const result = JSON.parse(this.copyCStr(this.instance.search(this.newString(search_str))));
+      if (result.status === 'OK') {
+        const data = JSON.parse(result.data);
+        return data.map((item) => item.id);
+      } else {
+        console.error(result.reason);
+        return null;
+      }
+    } catch (e) {
+      console.error(e);
+      return null;
+    }
+  }
+
+  search_in (node_id, search_str) {
+    try {
+      const result = JSON.parse(this.copyCStr(this.instance.search_in(node_id, this.newString(search_str))));
+      if (result.status === 'OK') {
+        const data = JSON.parse(result.data);
+        return data.map((item) => item.id);
+      } else {
+        console.error(result.reason);
+        return null;
+      }
+    } catch (e) {
+      console.error(e);
+      return null;
+    }
+  }
+
+  query (query_str) {
+    try {
+      const result = JSON.parse(this.copyCStr(this.instance.query(this.newString(query_str))));
+      if (result.status === 'ERR') console.error(result.reason);
+      return (result.status === 'OK') ? result.data : null;
+    } catch (e) {
+      console.error(e);
+      return null;
+    }
+  }
+
+  query_on (node_id, query_str) {
+    try {
+      const result = JSON.parse(this.copyCStr(this.instance.query_on(node_id, this.newString(query_str))));
+      if (result.status === 'ERR') console.error(result.reason);
+      return (result.status === 'OK') ? result.data : null;
+    } catch (e) {
+      console.error(e);
+      return null;
+    }
+  }
+
+  insert (node_id, position, data_str) {
+    try {
+      const result = JSON.parse(this.copyCStr(this.instance.insert(node_id, position, this.newString(data_str))));
+      if (result.status === 'ERR') console.error(result.reason);
+      return (result.status === 'OK' && result.data);
+    } catch (e) {
+      console.error(e);
+      return false;
+    }
+  }
+
+  remove (node_id) {
+    try {
+      const result = JSON.parse(this.copyCStr(this.instance.remove(node_id)));
+      if (result.status === 'ERR') console.error(result.reason);
+      return (result.status === 'OK' && result.data);
+    } catch (e) {
+      console.error(e);
+      return false;
+    }
+  }
+
+  replace (node_id, data_str) {
+    try {
+      const result = JSON.parse(this.copyCStr(this.instance.replace(node_id, this.newString(data_str))));
+      if (result.status === 'ERR') console.error(result.reason);
+      return (result.status === 'OK' && result.data);
+    } catch (e) {
+      console.error(e);
+      return false;
+    }
+  }
+
+  is_child (parent_id, child_id) {
+    try {
+      const result = JSON.parse(this.copyCStr(this.instance.is_child(parent_id, child_id)));
+      if (result.status === 'ERR') {
+        console.error(result.reason);
+        return null;
+      }
+
+      return (result.status === 'OK' && result.data);
+    } catch (e) {
+      console.error(e);
+      return null;
+    }
+  }
+
+  print () {
+    try {
+      const result = JSON.parse(this.copyCStr(this.instance.get_formatted_data()));
+      if (result.status === 'OK') console.log(result.data);
+      else console.error(result.reason);
+    } catch (e) {
+      console.error(e);
+    }
+  }
+
+
+  copyCStr (ptr) {
+    let orig_ptr = ptr;
+    const self = this;
+
+    const collectCString = function* () {
+      let memory = new Uint8Array(self.instance.memory.buffer);
+      while (memory[ptr] !== 0) {
+        if (memory[ptr] === undefined) { throw new Error("Tried to read undef mem"); }
+        yield memory[ptr];
+        ptr += 1;
       }
     }
-  }`));
-  console.log(m.get_formatted_data());
 
-  const searchTypedArray = new Uint32Array(m.search("p").buffer);
-  const searchResults = Array.from(searchTypedArray).filter((i) => {
-    return i > 0;
-  });
-  console.log(searchResults);
+    const buffer_as_u8 = new Uint8Array(collectCString());
+    const utf8Decoder = new TextDecoder("UTF-8");
+    const buffer_as_utf8 = utf8Decoder.decode(buffer_as_u8);
+    self.instance.dealloc_str(orig_ptr);
+    return buffer_as_utf8;
+  }
 
-  const searchInTypedArray = new Uint32Array(m.search_in(BigInt(searchResults[0]), "attrs").buffer);
-  const searchInResults = Array.from(searchInTypedArray).filter((i) => {
-    return i > 0;
-  });
-  console.log(searchInResults);
-
-  console.log(m.stringify());
-
-  console.log(m.query("p"));
-
-  console.log(m.is_descendant(BigInt(searchResults[0]), BigInt(searchInResults[0])));
-
-  console.log(m.replace(BigInt(searchResults[1]), `{
-    "ul": {
-      "li": {
-        "text": "World"
+  getStr (ptr, len) {
+    const getData = function* (ptr, len) {
+      let memory = new Uint8Array(self.instance.memory.buffer);
+      for (let index = 0; index < len; index++) {
+        if (memory[ptr] === undefined) { throw new Error(`Tried to read undef mem at ${ptr}`); }
+        yield memory[ptr + index];
       }
     }
-  }`));
 
-  console.log(m.get_formatted_data());
-}).catch(console.error);
+    const buffer_as_u8 = new Uint8Array(getData(ptr/8, len/8));
+    const utf8Decoder = new TextDecoder("UTF-8");
+    const buffer_as_utf8 = utf8Decoder.decode(buffer_as_u8);
+    return buffer_as_utf8;
+  }
+
+  newString (str) {
+    const utf8Encoder = new TextEncoder("UTF-8");
+    let string_buffer = utf8Encoder.encode(str);
+    let len = string_buffer.length;
+    let ptr = this.instance.alloc(len+1);
+
+    let memory = new Uint8Array(this.instance.memory.buffer);
+    for (let i = 0; i < len; i++) {
+      memory[ptr+i] = string_buffer[i];
+    }
+
+    memory[ptr+len] = 0;
+
+    return ptr;
+  }
+}
+
+module.exports = Hson;
--- src/lib.rs
+++ src/lib.rs
@@ -1,169 +1,458 @@
-#[macro_use]
-extern crate lazy_static;
-
-extern crate js_sys;
-extern crate wasm_bindgen;
+#[macro_use] extern crate lazy_static;
+#[macro_use] extern crate serde_derive;
+#[macro_use] extern crate serde_json;
+extern crate serde;
 extern crate hson;
 
+use std::panic;
+use std::os::raw::*;
+use std::mem;
+use std::ffi::{CString, CStr};
+use serde_json::Value;
 use std::sync::Mutex;
-use js_sys::*;
-use wasm_bindgen::prelude::*;
 use hson::{ Hson, Ops, Search, Cast, Debug };
 
 lazy_static! {
     static ref HSON: Mutex<Hson> = Mutex::new(Hson::new());
 }
 
-#[wasm_bindgen]
-extern "C" {
-    type Property;
-
-    #[wasm_bindgen(method, getter)]
-    fn value (this: &Property) -> String;
-
-    #[wasm_bindgen(method, setter)]
-    fn set_value (this: &Property, val: String);
+extern {
+    fn console(ptr: *const c_char);
 }
 
+#[no_mangle]
+pub extern fn parse (data: *const c_char) -> *const c_char {
+    let data = unsafe {
+        match CStr::from_ptr(data).to_str() {
+            Ok(s) => s,
+            Err(e) => return wrap(to_error(e.to_string()))
+        }
+    };
 
-#[wasm_bindgen]
-pub fn parse (data: &str) -> Result<bool, JsValue> {
     match HSON.lock().unwrap().parse(data) {
-        Ok(()) => Ok(true),
-        Err(e) => Err(JsValue::from_str(&format!("{}", e)))
+        Ok(()) => wrap(to_success(Value::Bool(true))),
+        Err(e) => wrap(to_error(e.to_string()))
     }
 }
 
-#[wasm_bindgen]
-pub fn stringify () -> String {
-    HSON.lock().unwrap().stringify()
+#[no_mangle]
+pub extern fn stringify () -> *const c_char {
+    wrap(to_success(Value::String(HSON.lock().unwrap().stringify())))
 }
 
-#[wasm_bindgen]
-pub fn search (query: &str) -> Vec<u64> {
+#[no_mangle]
+pub extern fn search (query: *const c_char) -> *const c_char {
+    #[derive(Serialize, Deserialize)]
+    struct Ids { id: u64 };
+
+    let query = unsafe {
+        match CStr::from_ptr(query).to_str() {
+            Ok(s) => s,
+            Err(e) => return wrap(to_error(e.to_string()))
+        }
+    };
+
     match HSON.lock().unwrap().search(query) {
-        Ok(v) => v,
-        Err(_e) => Vec::new()
+        Ok(v) => {
+            let to_struct = v.iter().map(|id| Ids { id: *id }).collect::<Vec<Ids>>();
+            wrap(to_success(Value::String(serde_json::to_string(&to_struct).unwrap())))
+        },
+        Err(e) => return wrap(to_error(e.to_string()))
     }
 }
 
-#[wasm_bindgen]
-pub fn search_in (node_id: u64, query: &str) -> Vec<u64> {
-    match HSON.lock().unwrap().search_in(node_id, query) {
-        Ok(v) => v,
-        Err(_e) => Vec::new()
+#[no_mangle]
+pub extern fn search_in (node_id: c_int, query: *const c_char) -> *const c_char {
+    #[derive(Serialize, Deserialize)]
+    struct Ids { id: u64 };
+
+    let query = unsafe {
+        match CStr::from_ptr(query).to_str() {
+            Ok(s) => s,
+            Err(e) => return wrap(to_error(e.to_string()))
+        }
+    };
+
+    match HSON.lock().unwrap().search_in(node_id as u64, query) {
+        Ok(v) => {
+            let to_struct = v.iter().map(|id| Ids { id: *id }).collect::<Vec<Ids>>();
+            wrap(to_success(Value::String(serde_json::to_string(&to_struct).unwrap())))
+        },
+        Err(e) => return wrap(to_error(e.to_string()))
     }
 }
 
-#[wasm_bindgen]
-pub fn query (query: &str) -> Vec<JsValue> {
+#[no_mangle]
+pub extern fn query (query: *const c_char) -> *const c_char {
+    #[derive(Serialize, Deserialize)]
+    struct JsObj {
+        id: u64,
+        key: String,
+        value: String,
+        kind: String,
+        parent: u64,
+        childs: Vec<u64>
+    };
     let mut results = Vec::new();
     let mut h = HSON.lock().unwrap();
+    let query = unsafe {
+        match CStr::from_ptr(query).to_str() {
+            Ok(s) => s,
+            Err(e) => return wrap(to_error(e.to_string()))
+        }
+    };
 
     match h.search(query) {
         Ok(v) => {
             for id in v {
                 if let Some(vx) = h.get_vertex(id) {
-                    let target = Object::new();
+                    let target = JsObj {
+                        id: vx.id,
+                        key: vx.key_as_string().unwrap(),
+                        value: vx.value_as_string().unwrap(),
+                        kind: format!("{:?}", vx.kind),
+                        parent: vx.parent,
+                        childs: vx.childs
+                    };
 
-                    set_int_property(&target, "id", vx.id);
-                    set_str_property(&target, "key", &vx.key_as_string().unwrap());
-                    set_str_property(&target, "value", &vx.value_as_string().unwrap());
-                    set_str_property(&target, "type", &format!("{:?}", vx.kind));
-                    set_int_property(&target, "parent", vx.parent);
-                    set_str_property(&target, "childs", &vx.childs.iter()
-                                                                            .map(|i| i.to_string())
-                                                                            .collect::<Vec<String>>()
-                                                                            .join(","));
-
-                    results.push(JsValue::from(target));
+                    results.push(serde_json::to_value(target).unwrap());
                 }
             }
 
-            results
+            wrap(to_success(serde_json::to_value(results).unwrap()))
         },
-        Err(_e) => results
+        Err(e) => return wrap(to_error(e.to_string()))
     }
 }
 
-#[wasm_bindgen]
-pub fn query_on (node_id: u64, query: &str) -> Vec<JsValue> {
+#[no_mangle]
+pub extern fn query_on (node_id: c_int, query: *const c_char) -> *const c_char {
+    #[derive(Serialize, Deserialize)]
+    struct JsObj {
+        id: u64,
+        key: String,
+        value: String,
+        kind: String,
+        parent: u64,
+        childs: Vec<u64>
+    };
     let mut results = Vec::new();
     let mut h = HSON.lock().unwrap();
+    let query = unsafe {
+        match CStr::from_ptr(query).to_str() {
+            Ok(s) => s,
+            Err(e) => return wrap(to_error(e.to_string()))
+        }
+    };
 
-    match h.search_in(node_id, query) {
+    match h.search_in(node_id as u64, query) {
         Ok(v) => {
             for id in v {
                 if let Some(vx) = h.get_vertex(id) {
-                    let target = Object::new();
+                    let target = JsObj {
+                        id: vx.id,
+                        key: vx.key_as_string().unwrap(),
+                        value: vx.value_as_string().unwrap(),
+                        kind: format!("{:?}", vx.kind),
+                        parent: vx.parent,
+                        childs: vx.childs
+                    };
 
-                    set_int_property(&target, "id", vx.id);
-                    set_str_property(&target, "key", &vx.key_as_string().unwrap());
-                    set_str_property(&target, "value", &vx.value_as_string().unwrap());
-                    set_str_property(&target, "type", &format!("{:?}", vx.kind));
-                    set_int_property(&target, "parent", vx.parent);
-                    set_str_property(&target, "childs", &vx.childs.iter()
-                                                                            .map(|i| i.to_string())
-                                                                            .collect::<Vec<String>>()
-                                                                            .join(","));
-
-                    results.push(JsValue::from(target));
+                    results.push(serde_json::to_value(target).unwrap());
                 }
             }
 
-            results
+            wrap(to_success(serde_json::to_value(results).unwrap()))
         },
-        Err(_e) => results
+        Err(e) => return wrap(to_error(e.to_string()))
     }
 }
 
-#[wasm_bindgen]
-pub fn insert (node_id: u64, idx: usize, s: &str) -> Result<bool, JsValue> {
-    match HSON.lock().unwrap().insert(node_id, idx, s) {
-        Ok(()) => Ok(true),
-        Err(e) => Err(JsValue::from_str(&format!("{}", e)))
+#[no_mangle]
+pub extern fn insert (node_id: c_int, insert_pos: c_uint, data_to_insert: *const c_char) -> *const c_char {
+    let data_to_insert = unsafe {
+        match CStr::from_ptr(data_to_insert).to_str() {
+            Ok(s) => s,
+            Err(e) => return wrap(to_error(e.to_string()))
+        }
+    };
+
+    match HSON.lock().unwrap().insert(node_id as u64, insert_pos as usize, data_to_insert) {
+        Ok(()) => wrap(to_success(Value::Bool(true))),
+        Err(e) => wrap(to_error(e.to_string()))
     }
 }
 
-#[wasm_bindgen]
-pub fn remove (node_id: u64) -> Result<bool, JsValue> {
-    match HSON.lock().unwrap().remove(node_id) {
-        Ok(()) => Ok(true),
-        Err(e) => Err(JsValue::from_str(&format!("{}", e)))
+#[no_mangle]
+pub extern fn remove (node_id: c_int) -> *const c_char {
+    match HSON.lock().unwrap().remove(node_id as u64) {
+        Ok(()) => wrap(to_success(Value::Bool(true))),
+        Err(e) => wrap(to_error(e.to_string()))
     }
 }
 
-#[wasm_bindgen]
-pub fn replace (node_id: u64, data_to_insert: &str) -> Result<bool, JsValue> {
-    match HSON.lock().unwrap().replace(node_id, data_to_insert) {
-        Ok(()) => Ok(true),
-        Err(e) => Err(JsValue::from_str(&format!("{}", e)))
+#[no_mangle]
+pub extern fn replace (node_id: c_int, data_to_insert: *const c_char) -> *const c_char {
+    let data_to_insert = unsafe {
+        match CStr::from_ptr(data_to_insert).to_str() {
+            Ok(s) => s,
+            Err(e) => return wrap(to_error(e.to_string()))
+        }
+    };
+
+    match HSON.lock().unwrap().replace(node_id as u64, data_to_insert) {
+        Ok(()) => wrap(to_success(Value::Bool(true))),
+        Err(e) => wrap(to_error(e.to_string()))
     }
 }
 
-#[wasm_bindgen]
-pub fn is_descendant (node_id: u64, child_id: u64) -> bool {
-    HSON.lock().unwrap().is_descendant(node_id, child_id)
+#[no_mangle]
+pub extern fn is_child (parent_id: c_int, child_id: c_int) -> *const c_char {
+    wrap(to_success(Value::Bool(HSON.lock().unwrap().is_descendant(parent_id as u64, child_id as u64))))
 }
 
-#[wasm_bindgen]
-pub fn get_formatted_data () -> String {
-    HSON.lock().unwrap().get_formatted_data()
+#[no_mangle]
+pub extern fn get_formatted_data () -> *const c_char {
+    wrap(to_success(Value::String(HSON.lock().unwrap().get_formatted_data())))
+}
+
+#[no_mangle]
+pub extern fn alloc (size: usize) -> *mut c_void {
+    let mut buf = Vec::with_capacity(size);
+    let ptr = buf.as_mut_ptr();
+    mem::forget(buf);
+    return ptr as *mut c_void;
+}
+
+#[no_mangle]
+pub extern fn dealloc (ptr: *mut c_void, cap: usize) {
+    unsafe  {
+        let _buf = Vec::from_raw_parts(ptr, 0, cap);
+    }
+}
+
+#[no_mangle]
+pub extern fn dealloc_str (ptr: *mut c_char) {
+    unsafe {
+        let _ = CString::from_raw(ptr);
+    }
+}
+
+#[no_mangle]
+pub extern fn debug () {
+    panic::set_hook(Box::new(|panic_info| {
+        if let Some(s) = panic_info.payload().downcast_ref::<String>() {
+            unsafe {
+                console(wrap(to_error(s.clone())));
+            }
+        } else if let Some(s) = panic_info.payload().downcast_ref::<&str>() {
+            unsafe {
+                console(wrap(to_error(s.to_string())));
+            }
+        } else {
+            unsafe {
+                console(wrap(to_error(String::from("Panic occurred"))));
+            }
+        }
+
+        if let Some(l) = panic_info.location() {
+            unsafe {
+                console(wrap(to_error(format!("{:?}", l))));
+            }
+        }
+    }));
+}
+
+fn to_success (data: Value) -> String {
+    #[derive(Serialize, Deserialize)]
+    struct SuccessResponse {
+        status: String,
+        data: Value
+    }
+
+    let s = SuccessResponse {
+        status: String::from("OK"),
+        data
+    };
+
+    serde_json::to_string(&s).unwrap()
+}
+
+fn to_error (reason: String) -> String {
+    #[derive(Serialize, Deserialize)]
+    struct ErrResponse {
+        status: String,
+        reason: String
+    }
+
+    let e = ErrResponse {
+        status: String::from("ERR"),
+        reason
+    };
+
+    serde_json::to_string(&e).unwrap()
+}
+
+fn wrap (v: String) -> *const c_char {
+    CString::new(v.as_bytes()).unwrap().into_raw()
 }
 
 
-fn set_str_property (target: &Object, key: &str, val: &str) -> Object {
-    let key = JsValue::from_str(key);
-    let value = Property::from(JsValue::from(Object::new()));
-    value.set_value(val.to_string());
-    let descriptor = Object::from(JsValue::from(value));
-    Object::define_property(target, &key, &descriptor)
-}
 
-fn set_int_property (target: &Object, key: &str, val: u64) -> Object {
-    let key = JsValue::from_str(key);
-    let value = Property::from(JsValue::from(Object::new()));
-    value.set_value(val.to_string());
-    let descriptor = Object::from(JsValue::from(value));
-    Object::define_property(target, &key, &descriptor)
-}
+
+
+
+
+//#[wasm_bindgen]
+//extern "C" {
+//    type Property;
+//
+//    #[wasm_bindgen(method, getter)]
+//    fn value (this: &Property) -> String;
+//
+//    #[wasm_bindgen(method, setter)]
+//    fn set_value (this: &Property, val: String);
+//}
+//
+//
+//#[wasm_bindgen]
+//pub fn parse (data: &str) -> Result<bool, JsValue> {
+//    match HSON.lock().unwrap().parse(data) {
+//        Ok(()) => Ok(true),
+//        Err(e) => Err(JsValue::from_str(&format!("{}", e)))
+//    }
+//}
+//
+//#[wasm_bindgen]
+//pub fn stringify () -> String {
+//    HSON.lock().unwrap().stringify()
+//}
+//
+//#[wasm_bindgen]
+//pub fn search (query: &str) -> Vec<u64> {
+//    match HSON.lock().unwrap().search(query) {
+//        Ok(v) => v,
+//        Err(_e) => Vec::new()
+//    }
+//}
+//
+//#[wasm_bindgen]
+//pub fn search_in (node_id: u64, query: &str) -> Vec<u64> {
+//    match HSON.lock().unwrap().search_in(node_id, query) {
+//        Ok(v) => v,
+//        Err(_e) => Vec::new()
+//    }
+//}
+//
+//#[wasm_bindgen]
+//pub fn query (query: &str) -> Vec<JsValue> {
+//    let mut results = Vec::new();
+//    let mut h = HSON.lock().unwrap();
+//
+//    match h.search(query) {
+//        Ok(v) => {
+//            for id in v {
+//                if let Some(vx) = h.get_vertex(id) {
+//                    let target = Object::new();
+//
+//                    set_int_property(&target, "id", vx.id);
+//                    set_str_property(&target, "key", &vx.key_as_string().unwrap());
+//                    set_str_property(&target, "value", &vx.value_as_string().unwrap());
+//                    set_str_property(&target, "type", &format!("{:?}", vx.kind));
+//                    set_int_property(&target, "parent", vx.parent);
+//                    set_str_property(&target, "childs", &vx.childs.iter()
+//                                                                            .map(|i| i.to_string())
+//                                                                            .collect::<Vec<String>>()
+//                                                                            .join(","));
+//
+//                    results.push(JsValue::from(target));
+//                }
+//            }
+//
+//            results
+//        },
+//        Err(_e) => results
+//    }
+//}
+//
+//#[wasm_bindgen]
+//pub fn query_on (node_id: u64, query: &str) -> Vec<JsValue> {
+//    let mut results = Vec::new();
+//    let mut h = HSON.lock().unwrap();
+//
+//    match h.search_in(node_id, query) {
+//        Ok(v) => {
+//            for id in v {
+//                if let Some(vx) = h.get_vertex(id) {
+//                    let target = Object::new();
+//
+//                    set_int_property(&target, "id", vx.id);
+//                    set_str_property(&target, "key", &vx.key_as_string().unwrap());
+//                    set_str_property(&target, "value", &vx.value_as_string().unwrap());
+//                    set_str_property(&target, "type", &format!("{:?}", vx.kind));
+//                    set_int_property(&target, "parent", vx.parent);
+//                    set_str_property(&target, "childs", &vx.childs.iter()
+//                                                                            .map(|i| i.to_string())
+//                                                                            .collect::<Vec<String>>()
+//                                                                            .join(","));
+//
+//                    results.push(JsValue::from(target));
+//                }
+//            }
+//
+//            results
+//        },
+//        Err(_e) => results
+//    }
+//}
+//
+//#[wasm_bindgen]
+//pub fn insert (node_id: u64, idx: usize, s: &str) -> Result<bool, JsValue> {
+//    match HSON.lock().unwrap().insert(node_id, idx, s) {
+//        Ok(()) => Ok(true),
+//        Err(e) => Err(JsValue::from_str(&format!("{}", e)))
+//    }
+//}
+//
+//#[wasm_bindgen]
+//pub fn remove (node_id: u64) -> Result<bool, JsValue> {
+//    match HSON.lock().unwrap().remove(node_id) {
+//        Ok(()) => Ok(true),
+//        Err(e) => Err(JsValue::from_str(&format!("{}", e)))
+//    }
+//}
+//
+//#[wasm_bindgen]
+//pub fn replace (node_id: u64, data_to_insert: &str) -> Result<bool, JsValue> {
+//    match HSON.lock().unwrap().replace(node_id, data_to_insert) {
+//        Ok(()) => Ok(true),
+//        Err(e) => Err(JsValue::from_str(&format!("{}", e)))
+//    }
+//}
+//
+//#[wasm_bindgen]
+//pub fn is_descendant (node_id: u64, child_id: u64) -> bool {
+//    HSON.lock().unwrap().is_descendant(node_id, child_id)
+//}
+//
+//#[wasm_bindgen]
+//pub fn get_formatted_data () -> String {
+//    HSON.lock().unwrap().get_formatted_data()
+//}
+//
+//
+//fn set_str_property (target: &Object, key: &str, val: &str) -> Object {
+//    let key = JsValue::from_str(key);
+//    let value = Property::from(JsValue::from(Object::new()));
+//    value.set_value(val.to_string());
+//    let descriptor = Object::from(JsValue::from(value));
+//    Object::define_property(target, &key, &descriptor)
+//}
+//
+//fn set_int_property (target: &Object, key: &str, val: u64) -> Object {
+//    let key = JsValue::from_str(key);
+//    let value = Property::from(JsValue::from(Object::new()));
+//    value.set_value(val.to_string());
+//    let descriptor = Object::from(JsValue::from(value));
+//    Object::define_property(target, &key, &descriptor)
+//}
